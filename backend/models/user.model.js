1947daa99adb660be9dd50509ca5a2d4f4f3b475
7390Harshil <harshil.gupta9999@gmail.com>
2025-05-10 20:44:23 +0530

    UserModel

---
+++ backend/models/user.model.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+    firstName : {
+        type : String,
+        required : true,
+    },
+
+    lastName : {
+        type : String,
+        required : true,
+    },
+
+    email: {
+        type: String,
+        required : true,
+        unique : true,
+    },
+
+    password:{
+        type : String,
+        required : true,
+    },
+
+    type : {
+        type : String,
+        enum : ['Patient' , 'Doctor'],
+        required : true,
+    },
+
+    isVerified : {
+        type : Boolean,
+        default : false,
+    }
+},
+{
+    timestamps : true,
+});
+
+const UserModel = mongoose.model('User' , 'UserSchema');
+export default UserModel;
