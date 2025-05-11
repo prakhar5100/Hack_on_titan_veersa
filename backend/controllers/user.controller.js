e0c97910c745a9cf6f5ee56ffdfa312709ac904b
7390Harshil <harshil.gupta9999@gmail.com>
2025-05-11 12:34:52 +0530

    Done

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -5,6 +5,7 @@ import jwt from 'jsonwebtoken';
 import User from '../models/user.model.js';
 import Patient from '../models/patient.model.js';
 import Doctor from '../models/doctor.model.js';
+import sendVerificationEmail from '../config/emailService.js' ;
 
 
 // Generating JWT Token
@@ -39,6 +40,14 @@ const registerUser = async (req, res) => {
         const newUser = new User(payload);
         const save = await newUser.save();
 
+        // Send Verification Email
+
+        const token = generateToken(newUser._id);
+        const verificationLink = `${process.env.FRONTEND_URL}/verify-email/${token}`;
+        await sendVerificationEmail(newUser.email , 'Verify your Email' , `Click the link to verify: ${verificationLink}`);
+        
+
+        //Check if the user is Patient or Doctor
         if (type === 'Patient') {
             if (!bloodGroup || !gender || !dateOfBirth) {
                 return res.status(400).json({
@@ -59,9 +68,28 @@ const registerUser = async (req, res) => {
         } 
         
         else if (type === 'Doctor') {
+            const { specializations, yearsOfExperience, consultationFee } = req.body;
+            
+            //Validate Doctor
+            if(!specializations || !yearsOfExperience || !consultationFee)
+            {
+                return res.status(400).json({
+                    message : 'Please provide specializations , yearsOfExperience ans consultationFee for a Doctor',
+                    error : true,
+                    success : false
+                });
+            }
             // Create Doctor
             const doctor = new Doctor({
                 userId: newUser._id,
+                specializations,
+                yearsOfExperience,
+                consultationFee,
+                medicalLicenseNumber,
+                clinicName,
+                clinicAddress,
+                bio,
+                languageSpoken,
             });
             await doctor.save();
         }
@@ -89,6 +117,7 @@ const LoginUser = async(req ,res) => {
     const { email , password } = req.body;
     try{
         const user = await User.findOne({ email });
+        
         // Check if user exists
         if(!user)
         {
@@ -99,6 +128,7 @@ const LoginUser = async(req ,res) => {
             });
         }
 
+        // Check if user is verified
         if(!user.isVerified)
         {
             return res.status(400).json({
@@ -118,6 +148,10 @@ const LoginUser = async(req ,res) => {
                 success : false
             });
         }
+
+        // Generate JWT Token
+        const token = generateToken(user._id);
+        res.json({ token });
     }
     catch(error)
     {
@@ -127,6 +161,45 @@ const LoginUser = async(req ,res) => {
             success : false
         });
     }
+};
+
+
+// Email Verification
+const verifyEmail = async (req , res) => {
+    const { token } = req.params;
+
+    try{
+        //Decoding token
+        const decoded = jwt.verify(token , process.env.JWT_SECRET);
+
+        //Finding user and verifying
+        const user = await User.findById(decoded.userId);
+        if(!user)
+        {
+            return res.status(400).json({
+                message : 'Invalid Token',
+                error : true,
+                success : flase
+            });
+        }
+
+        // Updating user to Verified
+        user.isVerified = true;
+        await user.save();
+        res.status(200).json({
+            message : 'Email verified Successfully',
+            error : false,
+            success : true
+        });
+    }
+    catch(error)
+    {
+        return res.status(500).json({
+            message : error.message || 'An error occured during email verification',
+            error : true,
+            success : false
+        });
+    }
 }
 
-export default {registerUser , LoginUser};
+export default {registerUser , LoginUser , verifyEmail};
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import userController from '../controllers/user.controller.js';
-const { registerUser, LoginUser } = userController;
+const { registerUser, LoginUser , verifyEmail } = userController;
 
 const router = express.Router();
 
 router.post('/register' , registerUser);
 router.post('/Login' , LoginUser);
+router.get('/verify-email/:token' , verifyEmail);
+
+
 export default router;
